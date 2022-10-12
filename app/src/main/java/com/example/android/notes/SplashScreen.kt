20ad6d2d1ff1f0a8dd39cd0d11dc49e870916255
Lacika <vargalacii111@gmail.com>
2022-10-12 13:28:29 +0200

    note

---
+++ app/src/main/java/com/example/android/notes/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.example.android.notes
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed( {
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 2000)
+
+    }
+}
---
+++ app/src/main/java/com/example/android/notes/dao/Dao.kt
@@ -0,0 +1,27 @@
+package com.example.android.notes.dao
+
+import androidx.room.*
+import androidx.room.Dao
+import com.example.android.notes.entities.Notes
+
+@Dao
+interface Dao {
+
+    @Query("SELECT * FROM notes ORDER BY id DESC")
+    suspend fun getAllNotes() : List<Notes>
+
+    @Query("SELECT * FROM notes WHERE id =:id")
+    suspend fun getSpecificNote(id:Int) : Notes
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNotes(note:Notes)
+
+    @Delete
+    suspend fun deleteNote(note:Notes)
+
+    @Query("DELETE FROM notes WHERE id =:id")
+    suspend fun deleteSpecificNote(id:Int)
+
+    @Update
+    suspend fun updateNote(note:Notes)
+}
